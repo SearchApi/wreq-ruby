81a4e9eb35c32bca4cc7c4f015aaa6d524e30956
0x676e67 <gngppz@gmail.com>
2025-12-03 17:36:31 +0800

    chore(request): streamline request responsibility modules

--- src/client/req.rs
+++ src/client/req.rs
@@ -1,16 +1,19 @@
-use std::net::IpAddr;
+use std::{net::IpAddr, time::Duration};
 
-use http::HeaderValue;
+use http::{HeaderValue, header};
 use indexmap::IndexMap;
 use magnus::{RHash, TryConvert, typed_data::Obj, value::ReprValue};
 use serde::Deserialize;
 use wreq::{
-    Proxy, Version,
+    Client, Proxy, Version,
     header::{HeaderMap, OrigHeaderMap},
 };
 
 use super::body::{Body, Json};
-use crate::{emulation::Emulation, extractor::Extractor};
+use crate::{
+    client::resp::Response, emulation::Emulation, error::wreq_error_to_magnus,
+    extractor::Extractor, http::Method, rt,
+};
 
 /// The parameters for a request.
 #[derive(Default, Deserialize)]
@@ -18,82 +21,82 @@ use crate::{emulation::Emulation, extractor::Extractor};
 pub struct Request {
     /// The emulation option for the request.
     #[serde(skip)]
-    pub emulation: Option<Emulation>,
+    emulation: Option<Emulation>,
 
     /// The proxy to use for the request.
     #[serde(skip)]
-    pub proxy: Option<Proxy>,
+    proxy: Option<Proxy>,
 
     /// Bind to a local IP Address.
-    pub local_address: Option<IpAddr>,
+    local_address: Option<IpAddr>,
 
     /// Bind to an interface by `SO_BINDTODEVICE`.
-    pub interface: Option<String>,
+    interface: Option<String>,
 
     /// The timeout to use for the request.
-    pub timeout: Option<u64>,
+    timeout: Option<u64>,
 
     /// The read timeout to use for the request.
-    pub read_timeout: Option<u64>,
+    read_timeout: Option<u64>,
 
     /// The HTTP version to use for the request.
     #[serde(skip)]
-    pub version: Option<Version>,
+    version: Option<Version>,
 
     /// The headers to use for the request.
     #[serde(skip)]
-    pub headers: Option<HeaderMap>,
+    headers: Option<HeaderMap>,
 
     /// The original headers to use for the request.
     #[serde(skip)]
-    pub orig_headers: Option<OrigHeaderMap>,
+    orig_headers: Option<OrigHeaderMap>,
 
     /// The option enables default headers.
-    pub default_headers: Option<bool>,
+    default_headers: Option<bool>,
 
     /// The cookies to use for the request.
     #[serde(skip)]
-    pub cookies: Option<Vec<HeaderValue>>,
+    cookies: Option<Vec<HeaderValue>>,
 
     /// Whether to allow redirects.
-    pub allow_redirects: Option<bool>,
+    allow_redirects: Option<bool>,
 
     /// The maximum number of redirects to follow.
-    pub max_redirects: Option<usize>,
+    max_redirects: Option<usize>,
 
     /// Sets gzip as an accepted encoding.
-    pub gzip: Option<bool>,
+    gzip: Option<bool>,
 
     /// Sets brotli as an accepted encoding.
-    pub brotli: Option<bool>,
+    brotli: Option<bool>,
 
     /// Sets deflate as an accepted encoding.
-    pub deflate: Option<bool>,
+    deflate: Option<bool>,
 
     /// Sets zstd as an accepted encoding.
-    pub zstd: Option<bool>,
+    zstd: Option<bool>,
 
     /// The authentication to use for the request.
-    pub auth: Option<String>,
+    auth: Option<String>,
 
     /// The bearer authentication to use for the request.
-    pub bearer_auth: Option<String>,
+    bearer_auth: Option<String>,
 
     /// The basic authentication to use for the request.
-    pub basic_auth: Option<(String, Option<String>)>,
+    basic_auth: Option<(String, Option<String>)>,
 
     /// The query parameters to use for the request.
-    pub query: Option<IndexMap<String, String>>,
+    query: Option<IndexMap<String, String>>,
 
     /// The form parameters to use for the request.
-    pub form: Option<IndexMap<String, String>>,
+    form: Option<IndexMap<String, String>>,
 
     /// The JSON body to use for the request.
-    pub json: Option<Json>,
+    json: Option<Json>,
 
     /// The body to use for the request.
     #[serde(skip)]
-    pub body: Option<Body>,
+    body: Option<Body>,
 }
 
 impl Request {
@@ -130,4 +133,116 @@ impl Request {
 
         Ok(builder)
     }
+}
+
+pub fn execute_request<U: AsRef<str>>(
+    client: Client,
+    method: Method,
+    url: U,
+    mut request: Request,
+) -> Result<Response, magnus::Error> {
+    rt::try_block_on(async move {
+        let mut builder = client.request(method.into_ffi(), url.as_ref());
+
+        // Emulation options.
+        apply_option!(set_if_some_inner, builder, request.emulation, emulation);
+
+        // Version options.
+        apply_option!(set_if_some, builder, request.version, version);
+
+        // Timeout options.
+        apply_option!(
+            set_if_some_map,
+            builder,
+            request.timeout,
+            timeout,
+            Duration::from_secs
+        );
+        apply_option!(
+            set_if_some_map,
+            builder,
+            request.read_timeout,
+            read_timeout,
+            Duration::from_secs
+        );
+
+        // Network options.
+        apply_option!(set_if_some, builder, request.proxy, proxy);
+        apply_option!(set_if_some, builder, request.local_address, local_address);
+        apply_option!(set_if_some, builder, request.interface, interface);
+
+        // Headers options.
+        apply_option!(set_if_some, builder, request.headers, headers);
+        apply_option!(set_if_some, builder, request.orig_headers, orig_headers);
+        apply_option!(
+            set_if_some,
+            builder,
+            request.default_headers,
+            default_headers
+        );
+
+        // Authentication options.
+        apply_option!(
+            set_if_some_map_ref,
+            builder,
+            request.auth,
+            auth,
+            AsRef::<str>::as_ref
+        );
+        apply_option!(set_if_some, builder, request.bearer_auth, bearer_auth);
+        if let Some(basic_auth) = request.basic_auth.take() {
+            builder = builder.basic_auth(basic_auth.0, basic_auth.1);
+        }
+
+        // Cookies options.
+        if let Some(cookies) = request.cookies.take() {
+            for cookie in cookies {
+                builder = builder.header_append(header::COOKIE, cookie);
+            }
+        }
+
+        // Allow redirects options.
+        match request.allow_redirects {
+            Some(false) => {
+                builder = builder.redirect(wreq::redirect::Policy::none());
+            }
+            Some(true) => {
+                builder = builder.redirect(
+                    request
+                        .max_redirects
+                        .take()
+                        .map(wreq::redirect::Policy::limited)
+                        .unwrap_or_default(),
+                );
+            }
+            None => {}
+        };
+
+        // Compression options.
+        apply_option!(set_if_some, builder, request.gzip, gzip);
+        apply_option!(set_if_some, builder, request.brotli, brotli);
+        apply_option!(set_if_some, builder, request.deflate, deflate);
+        apply_option!(set_if_some, builder, request.zstd, zstd);
+
+        // Query options.
+        apply_option!(set_if_some_ref, builder, request.query, query);
+
+        // Form options.
+        apply_option!(set_if_some_ref, builder, request.form, form);
+
+        // JSON options.
+        apply_option!(set_if_some_ref, builder, request.json, json);
+
+        // Body options.
+        if let Some(body) = request.body.take() {
+            builder = builder.body(wreq::Body::from(body));
+        }
+
+        // Send request.
+        builder
+            .send()
+            .await
+            .map(Response::new)
+            .map_err(wreq_error_to_magnus)
+    })
 }
