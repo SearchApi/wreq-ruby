80c6b83a979b1c6735213550a819d7e7005e051d
0x676e67 <gngppz@gmail.com>
2026-05-19 11:27:18 +0800

    test(emulate): Add chrome 148 test

--- test/emulate_test.rb
+++ test/emulate_test.rb
@@ -20,20 +20,25 @@ class EmulationTest < Minitest::Test
   end
 
   def test_http2_parser
-    str = File.read("test/results/chrome_147.json")
-    json = JSON.parse(str)
-    emulation = Wreq::Emulation.parse(JSON.dump(json), permute_extensions: true)
-    client = Wreq::Client.new(emulation: emulation)
-    resp = client.get("https://tls.peet.ws/api/all")
-    # ja4(no psk)
-    assert_includes resp.bytes, "t13d1516h2_8daaf6152771_d8a2da3f94cd"
-    # akamai
-    assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
 
-    resp = client.get("https://tls.peet.ws/api/all")
-    # ja4(psk)
-    assert_includes resp.bytes, "t13d1517h2_8daaf6152771_b6f405a00624"
-    # akamai
-    assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
+    profiles = ['test/results/chrome_147.json', 'test/results/chrome_148.json']
+
+    profiles.each do |profile|
+      str = File.read(profile)
+      json = JSON.parse(str)
+      emulation = Wreq::Emulation.parse(JSON.dump(json), permute_extensions: true)
+      client = Wreq::Client.new(emulation: emulation)
+      resp = client.get("https://tls.peet.ws/api/all")
+      # ja4(no psk)
+      assert_includes resp.bytes, "t13d1516h2_8daaf6152771_d8a2da3f94cd"
+      # akamai
+      assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
+
+      resp = client.get("https://tls.peet.ws/api/all")
+      # ja4(psk)
+      assert_includes resp.bytes, "t13d1517h2_8daaf6152771_b6f405a00624"
+      # akamai
+      assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
+    end
   end
 end
