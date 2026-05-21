b7d02475526b4c78283f5482892be9d1af8ada42
0x676e67 <gngppz@gmail.com>
2026-05-21 08:59:43 +0800

    test(emulate): update tests to support TLS/HTTP2 fingerprints for Edge and Opera

--- test/emulate_test.rb
+++ test/emulate_test.rb
@@ -40,4 +40,48 @@ class EmulationTest < Minitest::Test
       assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
     end
   end
+
+  def test_edge_parser
+    profiles = ["test/results/edge_148.json"]
+
+    profiles.each do |profile|
+      str = File.read(profile)
+      json = JSON.parse(str)
+      emulation = Wreq::Emulation.parse(JSON.dump(json), permute_extensions: true)
+      client = Wreq::Client.new(emulation: emulation)
+      resp = client.get("https://tls.peet.ws/api/all")
+      # ja4(no psk)
+      assert_includes resp.bytes, "t13d1516h2_8daaf6152771_d8a2da3f94cd"
+      # akamai
+      assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
+
+      resp = client.get("https://tls.peet.ws/api/all")
+      # ja4(psk)
+      assert_includes resp.bytes, "t13d1517h2_8daaf6152771_b6f405a00624"
+      # akamai
+      assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
+    end
+  end
+
+  def test_opera_parser
+    profiles = ["test/results/opera_131.json"]
+
+    profiles.each do |profile|
+      str = File.read(profile)
+      json = JSON.parse(str)
+      emulation = Wreq::Emulation.parse(JSON.dump(json), permute_extensions: true)
+      client = Wreq::Client.new(emulation: emulation)
+      resp = client.get("https://tls.peet.ws/api/all")
+      # ja4(no psk)
+      assert_includes resp.bytes, "t13d1516h2_8daaf6152771_d8a2da3f94cd"
+      # akamai
+      assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
+
+      resp = client.get("https://tls.peet.ws/api/all")
+      # ja4(psk)
+      assert_includes resp.bytes, "t13d1517h2_8daaf6152771_b6f405a00624"
+      # akamai
+      assert_includes resp.bytes, "52d84b11737d980aef856699f885ca86"
+    end
+  end
 end
