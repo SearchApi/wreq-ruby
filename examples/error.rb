282a768bd9978183aaa6a446328494a1eb8aa3a0
0x676e67 <gngppz@gmail.com>
2026-08-10 11:54:25 +0800

    feat(error): add detailed diagnostic messages (#160)

--- examples/error.rb
+++ examples/error.rb
@@ -5,8 +5,5 @@ require_relative "../lib/wreq"
 begin
   Wreq.get("not-a-valid-url")
 rescue Wreq::Error => error
-  puts "#{error.class}: #{error.message}"
-  puts "builder: #{error.builder?}"
-  puts "uri: #{error.uri.inspect}"
-  puts "status: #{error.status.inspect}"
+  warn error.full_message(highlight: false)
 end
